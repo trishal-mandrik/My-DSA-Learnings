d535786897b423a308d62108e6be5bdf3902478f
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-01-21 19:25:10 +0530

    LCA, Right-view, Left-view

---
+++ Binary_Tree/Left-view-of-BT.java
@@ -0,0 +1,19 @@
+package Binary_Tree;
+import java.util.*;
+
+class Solution {
+    void reversePreorder(TreeNode root, int level, List<Integer> ans){
+        if(root == null) return;
+        
+        if(level == ans.size()) ans.add(root.val);
+        
+        reversePreorder(root.left, level + 1, ans);
+        reversePreorder(root.right, level + 1, ans);
+    }
+        
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        reversePreorder(root, 0, ans);
+        return ans;
+    }
+}
