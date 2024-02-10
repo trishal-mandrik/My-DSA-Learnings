76f9c5531725361a5a53ae001588df3735d5553d
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-02-10 12:40:12 +0530

    BinarySearch

---
+++ Binary-Search/BinarySearch.java
@@ -0,0 +1,26 @@
+// package Binary-Search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int[] arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = start + (end - start) / 2;
+        int toFind = 3;
+
+        while (start <= end) {
+            if (arr[mid] == toFind) {
+                System.out.println("Found at index " + mid);
+                return;
+            } else if (toFind < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        System.out.println("not found");
+    }
+}
---
+++ Binary-Search/BookAllocation.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+public class Solution {
+    static boolean isPossibleSolution(ArrayList<Integer> arr, int n, int m, int mid){
+        int studentCount = 1;
+        int pageSum = 0;
+
+        for(int i = 0; i < n; i++){
+            if(pageSum + arr.get(i) <= mid){
+                pageSum += arr.get(i);
+            } else {
+                studentCount++;
+                if(studentCount > m || arr.get(i) > mid){
+                    return false;
+                }
+                pageSum = arr.get(i);
+            }
+        }
+        return true;
+    }
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // Write your code here.
+        int s = 0;
+        int ans = -1;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += arr.get(i);
+        }
+
+        int e = sum;
+        int mid;
+        while(s <= e){
+            mid = s + (e - s)/2;
+            if(isPossibleSolution(arr, n, m, mid)){
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
