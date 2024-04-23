7e9d08a3ae1f847942a01793316bfa5a3ce1b991
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-04-23 21:22:35 +0530

    RightViewOfBT, WidthOfBT

---
+++ Binary-Tree/RightViewOfBT.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class RightViewOfBT {
+    public void reversePreOrder(TreeNode root, int level, List<Integer> ans) {
+        if(root == null) return;
+
+        if(level == ans.size()) ans.add(root.val); 
+
+        reversePreOrder(root.right, level + 1, ans);
+        reversePreOrder(root.left, level + 1, ans);
+    }
+
+    public List<Integer> rightView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        reversePreOrder(root, 0, ans);
+        return ans;
+    }
+}
