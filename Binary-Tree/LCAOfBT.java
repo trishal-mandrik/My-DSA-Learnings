03140f8fb805921f4dd7846e1ff4a95889a6a370
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-05-11 12:19:31 +0530

    rootToNodePath and formatted file

--- Binary-Tree/LCAOfBT.java
+++ Binary-Tree/LCAOfBT.java
@@ -1,14 +1,3 @@
-package Binary_Tree;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
