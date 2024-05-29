58077173cbe3d2404f7015cbba93ae2ff8369028
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-05-29 10:20:47 +0530

    TwoSumBetter, TwoSumOptimal

---
+++ Array/TwoSumBetter.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+
+
+public class TwoSumBetter {
+    public static String read(int n, int []arr, int target){
+        // Write your code here.
+        Map<Integer, Integer> mpp = new TreeMap<>();
+        
+        for(int i = 0; i < n; i++){
+            int num = arr[i];
+            int moreNeeded = target - num;
+
+            if(mpp.containsKey(moreNeeded)) {
+                return "YES";
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return "NO";
+    }
+}
+
+// TC: O(NlogN) SC: O(N)
---
+++ Array/TwoSumOptimal.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Arrays;
+
+public class TwoSumOptimal {
+    public static String read(int n, int []arr, int target){
+        // Write your code here.
+        Arrays.sort(arr);
+        int left = 0, right = n-1;
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return "YES";
+            } else if (sum < target) left++;
+            else right--;
+        }
+        return "NO";
+    }
+}
+
+
+// TC: O(N) + O(NlogN) SC: O(1)
