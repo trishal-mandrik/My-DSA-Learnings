30dc3d79660a63d3fba896838aae22339005fc2b
Trishal Mandrik <trishal.mandrik@springernature.com>
2024-06-04 10:16:42 +0530

    ThreeSumOptimal

--- Array/ThreeSum/ThreeSumOptimal.java
+++ Array/ThreeSum/ThreeSumOptimal.java
@@ -1,5 +1,47 @@
 package Array.ThreeSum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreeSumOptimal {
-    
+    public List<List<Integer>> threeSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            // remove duplicates
+            if (i != 0 && nums[i] == nums[i - 1])  {
+                continue;
+            }
+
+            // moving 2 pointers
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    j++;
+                } else if (sum > target) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
 }
+
+
+// Time Complexity: O(NlogN)+O(N2), where N = size of the array.
+// Reason: The pointer i, is running for approximately N times. And both the pointers j and k combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N2). 
+
+// Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem. So, from that perspective, space complexity can be written as O(1).
