dcc91a81a952b3ea8f5ff3aa52f8bad6cd4f8210
trishalmandrik <trishal.mandrik@springernature.com>
2025-09-25 22:25:58 +0530

    SearchInRotatedSortedArray2

---
+++ BinarySearch/SearchInRotatedSortedArray2.java
@@ -0,0 +1,30 @@
+public class SearchInRotatedSortedArray2 {
+    public boolean search(int[] arr, int target) {
+        // code here
+        int low = 0, high = arr.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] == target) {
+                return true;
+            } else if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+            } else if (arr[low] <= arr[mid]) {
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if(arr[mid] <= target && target <= arr[high]){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
