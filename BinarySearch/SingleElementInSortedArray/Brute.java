fff5915a6d5e26c1aef6fb2d00ef0de4376a6d1a
trishalmandrik <trishal.mandrik@springernature.com>
2025-09-26 22:12:57 +0530

    SingleElementInSortedArray

---
+++ BinarySearch/SingleElementInSortedArray/Brute.java
@@ -0,0 +1,24 @@
+package SingleElementInSortedArray;
+
+public class Brute {
+    int single(int[] arr) {
+        // code here
+        int ind = 0;
+        int n = arr.length;
+        while(ind < n) {
+            if(ind + 1 < n) {
+                if(arr[ind] == arr[ind + 1]) {
+                    ind += 2;
+                } else {
+                    return arr[ind];
+                }
+            } else {
+                return arr[ind];
+            }
+        }
+        return -1;
+    }
+}
+
+// TC: O(N)
+// SC: O(1)
