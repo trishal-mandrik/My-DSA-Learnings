d182b2a946780e477c9685fef9a4e596e5ac63e2
trishalmandrik <trishal.mandrik@springernature.com>
2025-09-30 02:50:05 +0530

    SquareRoot

---
+++ BinarySearch/SquareRoot/Brute.java
@@ -0,0 +1,19 @@
+package SquareRoot;
+
+public class Brute {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        int ans = 1;
+        for(long i = 1; i <= x / 2; i++) {
+            if((i * i) <= x) {
+                ans = (int) i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+// TC: O(N)
+// SC: O(1)
---
+++ BinarySearch/SquareRoot/Optimal.java
@@ -0,0 +1,29 @@
+package SquareRoot;
+
+public class Optimal {
+    int floorSqrt(int x) {
+        // code here
+        if(x == 0) return 0;
+        int ans = 1;
+        long low = 1, high = x / 2;
+
+        while(low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if(mid * mid == x) {
+                ans = (int) mid;
+                return ans;
+            }
+            else if(mid * mid > x) {
+                high = mid - 1;
+            } else {
+                ans = (int) mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+
+// TC: O(logN)
+// SC: O(1)
